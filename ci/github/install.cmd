58e1771b2ac96115949e1fe99c714d30d5d3f676
Hyperblast <hypercode@zoho.com>
2021-09-26 08:12:19 +0300

    debug

--- ci/github/install.cmd
+++ ci/github/install.cmd
@@ -1,16 +1,16 @@
 setlocal
 cd "%~dp0..\.."
 
-scripts\install\patch.cmd
+call scripts\install\patch.cmd
 @if errorlevel 1 goto :end
 
-scripts\install\foobar2000.cmd v1.3
+call scripts\install\foobar2000.cmd v1.3
 @if errorlevel 1 goto :end
 
-scripts\install\foobar2000.cmd v1.4
+call scripts\install\foobar2000.cmd v1.4
 @if errorlevel 1 goto :end
 
-scripts\install\foobar2000.cmd v1.5
+call scripts\install\foobar2000.cmd v1.5
 @if errorlevel 1 goto :end
 
 :end
