76f766d8e1a8a7f9b836f96eb63e08f46386299d
Hyperblast <hypercode@zoho.com>
2021-09-26 07:45:03 +0300

    moar debug

--- scripts/install/foobar2000.cmd
+++ scripts/install/foobar2000.cmd
@@ -37,14 +37,18 @@ if errorlevel 1 goto :end
 cd %target_dir%
 if errorlevel 1 goto :end
 
+echo DEBUG: curl
 curl --fail --show-error --location -o %pkg_file% ^
     "https://hyperblast.org/files/foobar2000/%pkg_file%"
+echo DEBUG: curl: %errorlevel%
 if errorlevel 1 goto :end
 
 echo.>portable_mode_enabled
 if errorlevel 1 goto :end
 
+echo DEBUG: run installer
 %pkg_file% /S /D=%cd%
+echo DEBUG: run installer: %errorlevel%
 if errorlevel 1 goto :end
 
 del %pkg_file%
