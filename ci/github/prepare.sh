7a51ea86e1fce473feda72a3b8495f6cd3cc7c28
Hyperblast <hypercode@zoho.com>
2021-09-26 14:28:59 +0300

    minor fix

--- ci/github/prepare.sh
+++ ci/github/prepare.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$(dirname $0)/../.."
 
-docker build -t beefweb-main-dev -f docker-main
-docker build -t beefweb-oldlibc-dev -f docker-oldlibc
+docker build -t beefweb-main-dev -f docker-main .
+docker build -t beefweb-oldlibc-dev -f docker-oldlibc .
